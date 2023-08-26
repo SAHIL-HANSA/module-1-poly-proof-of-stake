62e2b590a67119f46391b24dd977c9a4d7a862d2
SAHIL-HANSA <106074469+SAHIL-HANSA@users.noreply.github.com>
2023-08-26 17:54:44 +0530

    Add files via upload

---
+++ nfts.js
@@ -0,0 +1,32 @@
+const nfts = [
+    [
+        {
+          "name": "1",
+          "description": "Anime 1",
+          "image": "https://gateway.pinata.cloud/ipfs/QmaBhXiJt1swDckcQdeLDrRpToiCr62vixoJsJoRFcFhKw/0.png"
+        },
+        {
+          "name": "2",
+          "description": "Anime 2",
+          "image": "https://gateway.pinata.cloud/ipfs/QmaBhXiJt1swDckcQdeLDrRpToiCr62vixoJsJoRFcFhKw/1.png"
+        },
+        {
+          "name": "3",
+          "description": "Anime 3",
+          "image": "https://gateway.pinata.cloud/ipfs/QmaBhXiJt1swDckcQdeLDrRpToiCr62vixoJsJoRFcFhKw/2.png"
+        },
+        {
+          "name": "4",
+          "description": "Anime 4",
+          "image": "https://gateway.pinata.cloud/ipfs/QmaBhXiJt1swDckcQdeLDrRpToiCr62vixoJsJoRFcFhKw/3.png"
+        },
+        {
+          "name": "5",
+          "description": "Anime 5",
+          "image": "https://gateway.pinata.cloud/ipfs/QmaBhXiJt1swDckcQdeLDrRpToiCr62vixoJsJoRFcFhKw/4.png"
+        }
+      ]
+      
+];
+
+module.exports = nfts;
